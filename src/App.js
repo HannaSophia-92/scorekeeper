9f586e45f284ec214b94525d6abade1fda278bc6
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-03-11 15:30:46 +0100

    Add decrement and increment function

--- src/App.js
+++ src/App.js
@@ -4,11 +4,29 @@ import CreateGamePage from "./pages/CreateGamePage";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import CreateGame from "./components/CreateGame";
+import Player from "./components/Player";
+import { useState } from "react";
 
 function App() {
+  const [players, setPlayers] = useState([
+    { name: "Player1", score: 0, id: "1" },
+    { name: "Player2", score: 0, id: "2" },
+    { name: "Player3", score: 0, id: "3" },
+    { name: "Player4", score: 0, id: "4" },
+  ]);
+
   return (
     <>
       <h1>scorekeeper</h1>
+      {players.map(({ name, score, id }, index) => (
+        <Player
+          name={name}
+          score={score}
+          onDecrement={() => decrementPlayerScore(index)}
+          onIncrement={() => incrementPlayerScore(index)}
+          key={id}
+        />
+      ))}
       <Routes>
         <Route path="/" element={<PlayPage />} />
         <Route path="/history" element={<HistoryPage />} />
@@ -18,6 +36,25 @@ function App() {
       <Navigation />
     </>
   );
+
+  function incrementPlayerScore(index) {
+    const player = players[index];
+
+    setPlayers([
+      ...players.slice(0, index),
+      { ...player, score: player.score + 1 },
+      ...players.slice(index + 1),
+    ]);
+  }
+
+  function decrementPlayerScore(index) {
+    const player = players[index];
+    setPlayers([
+      ...players.slice(0, index),
+      { ...player, score: player.score - 1 },
+      ...players.slice(index + 1),
+    ]);
+  }
 }
 
 export default App;
